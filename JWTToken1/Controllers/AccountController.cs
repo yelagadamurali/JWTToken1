f66bc77d479d9cd5a1667d47fcb4a9a01ec4bc58
DELL <DELL@DESKTOP-2TNLQAK>
2023-01-04 18:11:36 +0530

    Add project files.

---
+++ JWTToken1/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using JWTToken1.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTToken1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        [HttpPost("Authentication")]
+        public IActionResult Authentication([FromBody]AuthModel authModel)
+        {
+            if(string.IsNullOrEmpty(authModel.Username) || string.IsNullOrEmpty(authModel.Password))
+                return Unauthorized();
+            if(authModel.Username == authModel.Password)
+            {
+                //authentication is success
+              var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("security@123456789");
+                //tokenHandler.CreateToken();
+                var tokenDescriptor = new SecurityTokenDescriptor()
+                {
+                    //subject is nothinhbut payload
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, authModel.Username),
+                        new Claim(ClaimTypes.Role, "Admin"),
+                        new Claim(ClaimTypes.Role,"Murali")
+
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                                       
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var jwttoken = tokenHandler.WriteToken(token);
+                return Ok(jwttoken);
+            }
+            return Unauthorized();
+        }
+       
+       // [HttpGet("GetActionResult")]
+        [Authorize(Roles = "Murali")]
+        public IActionResult GetActionResult()
+        {
+            return Ok(new List<string>() { "Murali", "vineeth" });
+        }
+    }
+    
+}
---
+++ JWTToken1/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("security@123456789")),
+        ValidateIssuer = false,
+        ValidateAudience = false
+
+    };
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
